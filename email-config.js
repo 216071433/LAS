eec8c96b4988bf2c568f33ed145415f00a4e6861
MFANAMKHULU <139135865+MFANAMKHULU@users.noreply.github.com>
2025-05-27 11:09:37 +0200

    updated email service

--- email-config.js
+++ email-config.js
@@ -1,4 +1,8 @@
 // EmailJS configuration
+const EMAILJS_PUBLIC_KEY = 'crV0nuze0XtGcSVyZ';
+const EMAILJS_SERVICE_ID = 'service_mndoztz';
+const EMAILJS_TEMPLATE_ID = 'template_5iapevm';
+
 (function() {
     emailjs.init("YOUR_PUBLIC_KEY"); // You'll need to replace this with your actual EmailJS public key
 })(); 
